92b71e4ba706ce6c87beb1676133d41a1990c2d3
GajendraChoudhary <gajendrachoudhary18@gmail.com>
2024-04-22 12:41:52 +0530

    Adding Some Features

--- WP_Features/content_script.js
+++ WP_Features/content_script.js
@@ -1,17 +1,26 @@
 
-const blur = function (){
-let chatList = document.getElementById("pane-side");
-if(chatList){
-chatList.style.filter = "blur(4px)";
-}
+const blur = function () {
+    let chatList = document.getElementById("pane-side");
+    if (chatList) {
+        chatList.style.filter = "blur(3px)";
+    }
 };
 
 const checkElement = () => {
-    let chatList = document.getElementById("pane-side"); 
-    if(chatList){
+    let chatList = document.getElementById("pane-side");
+    if (chatList) {
         blur();
-    }else{
-        setTimeout(checkElement,1000);
+    } else {
+        setTimeout(checkElement, 1000);
     }
 }
+
 checkElement();
+const allChatToggle = document.getElementById("chat-panel").ischecked;
+const chatNames = document.getElementById("chat-names").value;
+const profileImage = document.getElementById("profile-image").value;
+console.log(allChatToggle, "Hello");
+// console.log(chatNames, "Hello");
+// console.log(profileImage, "Hello");
+
+
